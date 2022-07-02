ecc63281c877def4434128de0e079f6d30b26f77
unknown <dtnakhutsrishvili@gmail.com>
2022-07-03 01:55:22 +0400

    getting static styles for input form, tried to change color of placeholder * , came up with solution to make it with html and react state

--- src/pages/Components/Form.js
+++ src/pages/Components/Form.js
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import classes from "./Form.module.css";
 
 const Form = () => {
+
+// state for date placeholder
+ const [showDate,setShowDate]= useState(true)
+ const [name,setName]= useState("")
+console.log(name)
+
   return (
     <>
       <form className={classes.form}>
         <input
+        onChange={(e)=>{setName(e.target.value)}}
           type="text"
           className={classes.input}
           name="firstname"
@@ -15,7 +23,8 @@ const Form = () => {
 
         <input className={classes.input} type="number" name="phone" placeholder="Phone number *"></input>
 
-        <input className={classes.date} type="date" name="date" placeholder="Date of birth"></input>
+        <input  className={classes.date} type="date" required placeholder="Date"></input>
+       { showDate&& <p onClick={(e)=>{setShowDate(false)}} className={classes.dateText}>Date of birth <span className={classes.redStar}>*</span></p>}
       </form>
     </>
   );
