b5660c86944860f7a80c82cb767cdd039c0aa4a2
unknown <dtnakhutsrishvili@gmail.com>
2022-07-02 18:52:15 +0400

    second commit- creating personal information page and style them

--- src/pages/PersonalInfo.js
+++ src/pages/PersonalInfo.js
@@ -1,7 +1,58 @@
-const PersonalInfo=()=>{
-    return<>
-     <h1>PersonalInfo</h1>
-    </>
-}
+import classes from "./PersonalInfo.module.css";
+import king from "../images/king.png";
+import banner from "../images/banner.png";
+const PersonalInfo = () => {
+  return (
+    <>
+      <div className={classes.parent}>
+        <div>
+          <div className={classes.conteiner}>
+            <div className={classes.cupparent}>
+              <img className={classes.kingimg} src={king} alt="king"></img>
+              <p className={classes.cuptext}>Redberry Knight Cup</p>
+            </div>
+          </div>
 
-export default PersonalInfo
+          <img src={banner} alt="chess"></img>
+          <p className={classes.quate}>
+            “When you see a good move, look for a better one.”
+          </p>
+          <p className={classes.emanuel}>-Emanuel Lasker</p>
+        </div>
+        <div>
+          <div className={classes.headerDiv}>
+            <p className={classes.header}>Start creating your account</p>
+          </div>
+          <div className={classes.boxParent}>
+            <div className={classes.boxOne}>
+              <p className={classes.validate}>1</p>
+            </div>
+            <p className={classes.smPerInfo}>Personal information</p>
+            <div className={classes.line} />
+            <div className={classes.boxTwo}>
+              <p className={classes.secondValidate}>2</p>
+            </div>
+            <p className={classes.exp}>Chess experience</p>
+          </div>
+          <p className={classes.perInfo}>Personal Information</p>
+          <p className={classes.basicInfo}>This is basic informaton fields</p>
+
+
+          {/* <Form></Form> */}
+
+          {/* <form>
+          <input type="text" className={classes.name}  name="firstname" placeholder="Your name.."></input>
+
+          <input type="email"  name="email" placeholder="Your mail.."></input>
+
+          <input type="number"  name="phone" placeholder="Your phone.."></input>
+
+          <input type="date"  name="date" placeholder="Your date.."></input>
+          </form> */}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PersonalInfo;
